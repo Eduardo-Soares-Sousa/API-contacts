b8941bfc50329eabd26ab6fed483fff62c417edd
Eduardo-Soares-Sousa <eduardo_soares001@outlook.com>
2026-03-27 13:51:14 -0300

    Initial commit

---
+++ src/main/java/com/eduardo/apiContact/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.eduardo.apiContact.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtUtil jwtUttil;
+}
